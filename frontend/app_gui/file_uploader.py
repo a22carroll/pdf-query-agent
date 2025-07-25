836bf14835752c414d74c20f90a2d64ffe84ef4e
a22carroll <a22carroll@gmail.com>
2025-07-25 10:26:31 -0400

    working on file uploader

---
+++ frontend/app_gui/file_uploader.py
@@ -0,0 +1,7 @@
+import requests
+
+def upload_pdf(filepath, server_url="http://localhost:8000"):
+    with open(filepath, "rb") as f:
+        files = {'file': (filepath, f, 'application/pdf')}
+        response = requests.post(f"{server_url}/upload", files=files)
+    return response.json()
