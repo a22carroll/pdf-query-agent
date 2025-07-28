3d64c56d0b2d9774eedcea873a3c6cc2bcb5a4cd
Aidan Carroll <a22carroll@gmail.com>
2025-07-28 21:14:07 +0000

    added comments

--- backend/app/llm_interface.py
+++ backend/app/llm_interface.py
@@ -2,10 +2,13 @@ from transformers import pipeline
 
 generator = pipeline("text-generation", model="gpt2")
 
+#Preconditions: Takes in user question and Context Chunks
+#Post: returns Generated Anwser
 def generate_answer(question, context_chunks):
     context = "\n\n".join(context_chunks)
     prompt = f"Context:\n{context}\n\nQuestion: {question}\nAnswer:"
     
+    #Output parameters
     outputs = generator(
         prompt,
         max_length=512,
